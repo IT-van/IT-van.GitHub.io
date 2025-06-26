24a9e0f3d89cc1315419b08d4d3d6e0894b08e96
IT-van <ivanserebertsev2@gmail.com>
2025-06-26 17:19:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", function(){
+  const text = "Артём Станчук";
+  const typingSpeed = 150;
+  const delayAfterFinish = 3000;
+  const nameElement = document.getElementById("name");
+
+  let index = 1; 
+
+  function typeText() {
+    if (index <= text.length) {
+      nameElement.textContent = text.slice(0, index);
+      index++;
+      setTimeout(typeText, typingSpeed);
+    } else {
+      setTimeout(() => {
+        index = 1; 
+        nameElement.textContent = text[0]; 
+        typeText();
+      }, delayAfterFinish);
+    }
+  }
+  nameElement.textContent = text[0];
+  typeText();
+  $(".gallery").magnificPopup({
+    type: "image",
+    gallery:{
+      enabled: true
+    }
+  });
+  //document.addEventListener('DOMContentLoaded', function() {
+  //  const burger = document.getElementById('burger');
+  //  const menu = document.getElementById('menu');
+  //  
+  //  burger.addEventListener('click', function() {
+  //      this.classList.toggle('open');
+  //      menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
+  //  });
+  //});
+  document.addEventListener('DOMContentLoaded', function () {
+    const burger = document.getElementById('burger');
+    const menu = document.getElementById('menu');
+
+    burger.addEventListener('click', function () {
+      burger.classList.toggle('open');
+      menu.classList.toggle('active');
+    });
+  });
+});
